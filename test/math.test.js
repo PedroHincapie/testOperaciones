833f5c27cc019c6e62a5b40cdb2410161c5f657f
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2019-01-03 17:12:47 -0500

    Update math.test.js

--- test/math.test.js
+++ test/math.test.js
@@ -1,16 +1,37 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
+const assert = require('assert');
+const math = require("../math/math");
+
+describe('Math module', function() {
+  describe('@sum', function() {
+    it('should return 3 when params are 1 and 2', function() {
+      assert.equal(math.sum(1,2), 3);
+    });
+    it('should return -1 when params are 1 and -2', function() {
+      assert.equal(math.sum(1,-2), -1);
     });
   });
-});
-
-describe('Suma de numeros', function() {
-  describe('Suma', function() {
-    it('Suma exacta', function() {
-      assert.equal(3+3, 6);
+  describe('@diff', function() {
+    it('should return -1 when params are 1 and 2', function() {
+      assert.equal(math.rest(1,2), -1);
+    });
+    it('should return 4 when params are 2 and -2', function() {
+      assert.equal(math.rest(2,-2), 4);
+    });
+  });
+  describe('@product', function() {
+    it('should return 5 when params are 2 and 2.5', function() {
+      assert.equal(math.multiplication(2,2.5), 5);
+    });
+    it('should return 0 when params are 100 and 0', function() {
+      assert.equal(math.multiplication(100, 0), 0);
+    });
+  });
+  describe('@div', function() {
+    it('should return 2 when params are 6 and 3', function() {
+      assert.equal(math.division(6,3), 2);
+    });
+    it('should return Infinity when params are 1 and 0', function() {
+      assert.equal(math.division(1, 0), Infinity);
     });
   });
 });
