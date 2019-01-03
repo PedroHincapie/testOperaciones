bf87f3ec1393bbe7db59e2a31c3b2ab16b2afee7
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2019-01-03 17:12:36 -0500

    Ajustar la descripcion de las pruebas

--- math/math.js
+++ math/math.js
@@ -1,6 +1,6 @@
-module.export = {
-  suma : (a, b) => a+b,
-  resta : (a,b) => a-b,
-  multiplicacion : (a,b) => a * b,
+module.exports = {
+  sum : (a, b) => a+b,
+  rest : (a,b) => a-b,
+  multiplication : (a,b) => a * b,
   division : (a, b) => a / b
 }
